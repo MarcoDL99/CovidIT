866176eaa1e93c28e7a9c15052b32ab1d4559b2b
Lorediel <ld1@live.it>
2021-05-29 09:58:05 +0200

    Aggiunta della navigazione tra Italia e regioni, e regioni a screen provincia. Aggiunti gli svg di alcune regioni. Da aggiustare le dimensioni degli svg.

--- CovidIT/src/app/pages/italy/italy.page.ts
+++ CovidIT/src/app/pages/italy/italy.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { PopoverController} from '@ionic/angular';
 import { PopovermenuPage} from '../popovermenu/popovermenu.page';
+
 @Component({
   selector: 'app-italy',
   templateUrl: './italy.page.html',
@@ -14,12 +15,79 @@ export class ItalyPage implements OnInit {
    createMenu(event: Event){
     this.popover.create({event,component: PopovermenuPage, showBackdrop:false}).then((popoverElement)=>{popoverElement.present();});
    }
+
+   goToRegion(event: Event){
+    let s: any;
+    let nomeRegione = (<HTMLInputElement> event.target).getAttribute('title');
+    if (nomeRegione=="Abruzzo"){
+      s = "../assets/svg/regions/abruzzo.svg";
+    }
+    else if (nomeRegione=="Basilicata"){
+      s = "../assets/svg/regions/basilicata.svg";
+    }
+    else if (nomeRegione=="Calabria"){
+      s = "../assets/svg/regions/calabria.svg";
+    }
+    else if (nomeRegione=="Campania"){
+      s = "../assets/svg/regions/campania.svg";
+    }
+    else if (nomeRegione=="Emilia-Romagna"){
+      s = "../assets/svg/regions/emiliaRomagna.svg";
+    }
+    else if (nomeRegione=="Friuli-Venezia_Giulia"){
+      s = "../assets/svg/regions/friuliVeneziaGiulia.svg";
+    }
+    else if (nomeRegione=="Lazio"){
+      s = "../assets/svg/regions/lazio.svg";
+    }
+    else if (nomeRegione=="Liguria"){
+      s = "../assets/svg/regions/liguria.svg";
+    }
+    else if (nomeRegione=="Lombardia"){
+      s = "../assets/svg/regions/lombardia.svg";
+    }
+    else if (nomeRegione=="Marche"){
+      s = "../assets/svg/regions/marche.svg";
+    }
+    else if (nomeRegione=="Molise"){
+      s = "../assets/svg/regions/molise.svg";
+    }
+    else if (nomeRegione=="Piemonte"){
+      s = "../assets/svg/regions/piemonte.svg";
+    }
+    else if (nomeRegione=="Puglia"){
+      s = "../assets/svg/regions/puglia.svg";
+    }
+    else if (nomeRegione=="Sardegna"){
+      s = "../assets/svg/regions/sardegna.svg";
+    }
+    else if (nomeRegione=="Sicilia"){
+      s = "../assets/svg/regions/sicilia.svg";
+    }
+    else if (nomeRegione=="Toscana"){
+      s = "../assets/svg/regions/toscana.svg";
+    }
+    else if (nomeRegione=="Trentino-Alto Adige"){
+      s = "../assets/svg/regions/trentinoAltoAdige.svg";
+    }
+    else if (nomeRegione=="Umbria"){
+      s = "../assets/svg/regions/umbria.svg";
+    }
+    else if (nomeRegione=="Valle d'Aosta"){
+      s = "../assets/svg/regions/valleDAosta.svg";
+    }
+    else if (nomeRegione=="Veneto"){
+      s = "../assets/svg/regions/veneto.svg";
+    }
+    let NavigationExtras: NavigationExtras = {state: {regionSVG: s}};
+    this.router.navigate(['/region'], NavigationExtras);
+   }
+   
   ngOnInit() {
     //const sardinia = document.getElementById("sardinia");
     //sardinia.addEventListener("click", this.prova);
   }
 
-  prova(): void{
-    this.router.navigate(['/region']);
-  }
 }
+  
+
--- CovidIT/src/app/pages/region/region.page.ts
+++ CovidIT/src/app/pages/region/region.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PopoverController} from '@ionic/angular';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute, NavigationExtras} from '@angular/router';
 import {PopovermenuPage} from '../popovermenu/popovermenu.page';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-region',
@@ -10,12 +11,52 @@ import {PopovermenuPage} from '../popovermenu/popovermenu.page';
 })
 export class RegionPage implements OnInit {
 
-  constructor(private popover: PopoverController, private router: Router) {
+  region: any;
+  paths: any;
+
+  goToProvince(){
+    console.log("funziona");
   }
+
+  bindClick(){
+    let obj: any = document.getElementById("mapReg");
+    let svgDoc = obj.contentDocument;
+    this.paths = svgDoc.getElementsByTagName("path");
+    console.log(this.paths.length);
+    console.log("SI");
+    for (let i = 0; i < this.paths.length; i++) {
+      this.paths[i].addEventListener("click",this.goToProvince);
+    }
+  }
+
+  constructor(private sanitizer: DomSanitizer, private popover: PopoverController, private router: Router, private route: ActivatedRoute) {
+    
+     
+  }
+
   createMenu(event: Event){
     this.popover.create({event,component: PopovermenuPage, showBackdrop:false}).then((popoverElement)=>{popoverElement.present();});
   }
+
+
+
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.region = this.getSafeUrl(this.router.getCurrentNavigation().extras.state.regionSVG);
+      }
+     });
+    
   }
 
+  ionViewDidEnter(){
+    this.bindClick();
+  }
+
+
+  getSafeUrl(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
+}
+
+  
 }
