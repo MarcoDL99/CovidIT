b671c93d7f0c08d0e6826812d6cce1ad1a9d109f
Lorediel <ld1@live.it>
2021-06-07 15:59:31 +0200

    piccoli fix in region.model e provincia.service

--- CovidIT/src/app/services/provincia.service.ts
+++ CovidIT/src/app/services/provincia.service.ts
@@ -2,7 +2,7 @@ import {HttpClient} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import {Observable} from "rxjs";
 import {URL} from '../constants';
-import { ProvinciaModel } from "../model/provincia.model";
+import { Provincia } from "../model/provincia.model";
 
 
 @Injectable({
@@ -16,8 +16,8 @@ export class ProvinciaService{
     }
 
     //Restituisce i contagi della provincia selezionata
-    loadContagi(provincia: string): Observable<ProvinciaModel>{
-        return this.http.get<ProvinciaModel>(URL.PROVINCE + "/" + provincia);
+    loadContagi(provincia: string): Observable<Provincia>{
+        return this.http.get<Provincia>(URL.PROVINCE + "/" + provincia);
 
     }
 
