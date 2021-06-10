7a89e3f26043914d5331ec3910b971fdb9985d20
Lorediel <ld1@live.it>
2021-06-10 15:42:23 +0200

    Aggiunta configurazione server maven spring boot

---
+++ CovidIT_Server/src/test/java/it/univaq/progetto/CovidIT_Server/CovidItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.progetto.CovidIT_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
